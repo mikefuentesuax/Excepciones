d7653ded785d244593cedabeb7051f60d61f4943
JorgeRomojaro11 <jromodel@myuax.com>
2023-10-31 17:50:32 +0100

    Excepciones

---
+++ 3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdexcept>
+
+void func3() {
+}
+
+void func2() {
+    func3();
+}
+
+void func1() {
+    func2();
+}
+
+int main() {
+    try {
+        func1();
+    }
+    catch (const std::runtime_error& e) {
+        std::cout << "Excepción capturada en main: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
